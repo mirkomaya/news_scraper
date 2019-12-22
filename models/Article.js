7e77cce776d97922361bdf783cc1800ea1ecaa27
Marty McFly <mirko.maya@live.com>
2019-12-22 03:31:25 -0500

    Updated Article.js file.

--- models/Article.js
+++ models/Article.js
@@ -3,24 +3,32 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
+
     title: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
+
     summary: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
-    link: {
-        type: String,
-        required: true
+
+    saved: {
+        type: Boolean,
+        required: true,
+        default: false
     },
-    comment: {
-        type: Schema.Types.ObjectId,
-        ref: "Comment"
-    }
+
+    comment: [{
+        type: Schema.ObjectId,
+        ref: 'Comment'
+    }]
 });
 
 var Article = mongoose.model("Article", ArticleSchema);
 
+// Export the model
 module.exports = Article;
