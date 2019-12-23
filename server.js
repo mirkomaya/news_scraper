45e5bf7684c15218def342f4ce7f9096987f9892
Marty McFly <mirko.maya@live.com>
2019-12-22 23:17:24 -0500

    Added/deleted some routes in server.

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 var express = require("express");
-
-var logger = require("morgan");
-
 var mongoose = require("mongoose");
 
-var axios = require("axios");
+// var logger = require("morgan");
 
+var axios = require("axios");
 var cheerio = require("cheerio");
 
 var db = require("./models");
 
-var PORT = 4440;
+var PORT = process.env.PORT || 4440;
+
 
 var app = express();
 
-app.use(logger("dev"));
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -55,58 +55,64 @@ app.get("/scrape", function (req, res) {
 });
 
 app.get("/articles", function (req, res) {
+    // Grab every document in the Articles collection
     db.Article.find({})
         .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
             res.json(dbArticle);
         })
         .catch(function (err) {
+            // If an error occurred, send it to the client
             res.json(err);
         });
 });
 
 app.get("/articles/:id", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
-        .populate("comment")
         .then(function (dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
         })
         .catch(function (err) {
+            // If an error occurred, send it to the client
             res.json(err);
         });
 });
 
-// axios.get("https://www.nytimes.com/section/technology").then(function (response) {
+app.post("/articles/save/:id", function (req, res) {
+    // Use the article id to find and update its saved boolean
+    Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": true })
+        // Execute the above query
+        .exec(function (err, data) {
+            // Log any errors
+            if (err) {
+                console.log(err);
+            }
+            else {
 
-//     var $ = cheerio.load(response.data);
+                res.send(data);
+            }
+        });
+});
 
-//     var nyTimes = "https://www.nytimes.com";
+app.post("/articles/delete/:id", function (req, res) {
+    //Anything not saved
+    Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": false })
 
-//     var results = [];
-
-//     $("h2.css-1j9dxys").each(function (i, element) {
-
-//         var title = $(element).text();
-
-//         var summary = $(element).next().text();
-
-//         var link = `${nyTimes}${$(element).parent().attr("href")}`;
-
-//         results.push({
-//             title: title,
-//             summary: summary,
-//             link: link,
-//         });
-//     });
-
-//     console.log(results);
-//     console.log(results);
-
-// });
-// 
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
+        .exec(function (err, data) {
+            // Log any errors
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.send(data);
+            }
+        });
 });
 
 
-
-
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
